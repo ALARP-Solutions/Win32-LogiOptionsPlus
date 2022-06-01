2423c6da848a974e8d3728c3929354b18cb3f526
zyigo <morgan.wicks@icloud.com>
2022-06-01 19:32:29 +1000

    Final - Working

--- Build-Intunewin.ps1
+++ Build-Intunewin.ps1
@@ -1,4 +1,4 @@
-
+$vers = '1.0.5155'
 ## ------------------------------ ##
 ## Create Build Folder
 ## ------------------------------ ##
@@ -12,17 +12,14 @@ If (-Not (Test-Path $buildpath)) {
 ## ------------------------------ ##
 ## Create Temp Folder
 ## ------------------------------ ##
-
-$temppath = "$PSScriptRoot\temp"
+$tempfolder = "temp"
+$temppath = "$PSScriptRoot\$tempfolder"
 If (Test-Path $temppath) {
-    $Items = Get-ChildItem -LiteralPath "$temppath" -Recurse
-    foreach ($Item in $Items) {
-        $Item.Delete()
-    }
-    $Items = Get-Item -LiteralPath "$temppath"
-    $Items.Delete($true)
+    $relocation = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+    Move-Item $temppath $relocation
+    Remove-Item -LiteralPath "$relocation\$tempfolder" -Recurse -Force -Confirm:$false
 }
-New-Item -Path "$PSScriptRoot" -Name "temp" -ItemType "directory" | Out-Null
+New-Item $temppath -ItemType "directory" | Out-Null
 
 ## ------------------------------ ##
 ## Copy Inputs into Temp Folder
@@ -49,21 +46,56 @@ $wc.DownloadFile($url, $filepath)
 ## ------------------------------ ##
 ## Build the Intunewin File
 ## ------------------------------ ##
-$Testpath = "$buildpath\logioptionsplus_installer.intunewin"
+$Testpath = "$buildpath\LogiOptionsPlus.$vers.intunewin"
 if (Test-Path $Testpath) {
     Remove-Item $Testpath
 }
 
 & "$PSScriptRoot\Microsoft Win32 Content Prep Tool\IntuneWinAppUtil.exe" -c "$temppath" -s "$filepath" -o "$buildpath"
+Rename-Item -Path "$buildpath\logioptionsplus_installer.intunewin" -NewName "LogiOptionsPlus.$vers.intunewin"
 
 ## ------------------------------ ##
-## Clean-Up
+## Build the Detection Script
+## ------------------------------ ##
+$DSpath = "$buildpath\DetectionScript.$vers.ps1"
+if (Test-Path $DSpath) {
+    Remove-Item $DSpath
+}
+
+$detectionScript = @"
+`$FileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files\LogiOptionsPlus\logioptionsplus.exe").FileVersion
+#The below line trims the spaces before and after the version name
+`$FileVersion = `$FileVersion.Trim();
+if ([System.Version]`$FileVersion -ge [System.Version]'$vers') {
+    #Write the version to STDOUT by default
+    `$FileVersion
+    exit 0
+}
+else {
+    #Exit with non-zero failure code
+    exit 1
+}
+"@
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($DSpath, $detectionScript, $Utf8NoBomEncoding)
+
+# ## ------------------------------ ##
+# ## Clean-Up
+# ## ------------------------------ ##
+# If (Test-Path $temppath) {
+#     $Items = Get-ChildItem -LiteralPath "$temppath" -Recurse
+#     foreach ($Item in $Items) {
+#         $Item.Delete()
+#     }
+#     $Items = Get-Item -LiteralPath "$temppath"
+#     $Items.Delete($true)
+# }
+
+## ------------------------------ ##
+## Clean-Up - Pause here for debug
 ## ------------------------------ ##
 If (Test-Path $temppath) {
-    $Items = Get-ChildItem -LiteralPath "$temppath" -Recurse
-    foreach ($Item in $Items) {
-        $Item.Delete()
-    }
-    $Items = Get-Item -LiteralPath "$temppath"
-    $Items.Delete($true)
+    $relocation = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+    Move-Item $temppath $relocation
+    Remove-Item -LiteralPath "$relocation\$tempfolder" -Recurse -Force -Confirm:$false
 }
